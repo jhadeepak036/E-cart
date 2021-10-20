d495e8fad02bcf4059138792818b2c1fde6fe36e
Deepak M. Jha <77509618+jhadeepak036@users.noreply.github.com>
2021-10-20 19:50:13 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Apple',
+        price: 40000,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Old Car',
+        price: 500000,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '3',
+        name: 'Shoes',
+        price: 4000,
+        image: 'https://picsum.photos/id/21/3008/2008',
+      },
+    ],
+  };
+  export default data;
